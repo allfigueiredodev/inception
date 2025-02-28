d0ac8a7727cbdf80170f1edb7136a46936a9e3bc
allfigueiredodev <allfigueiredo@outlook.com>
2025-02-28 08:55:07 -0300

    "wip"

--- srcs/requirements/tools/set_up.sh
+++ srcs/requirements/tools/set_up.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ ! -d "${VOLUMES_PATH}" ]; then
         echo "Creating data folder"
-        echo "TESTTEEEE"
         sudo chmod -R 0777 /home
         mkdir -p ${VOLUMES_PATH}/mariadb
         mkdir -p ${VOLUMES_PATH}/wordpress
@@ -9,9 +8,9 @@ fi
 
 if [ ! -d "/home/${LOGIN}/var" ]; then
         echo "Creating var folder"
-        mkdir -p ~/var/www/html
+        mkdir -p /home/${LOGIN}/var/www/html
 fi
 
 if ! grep -q "localhost ${LOGIN}.42.fr" /etc/hosts; then \
-		sudo sed -i "2i\127.0.0.1\tlocalhost ${LOGIN}.42.fr" /etc/hosts; \
+		sudo sed -i "2i\127.0.0.1\tlocalhost ${LOGIN}.42.fr" /etc/hosts; 
 fi
--- srcs/requirements/wordpress/conf/my_script.sh
+++ srcs/requirements/wordpress/conf/my_script.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
 wp config create --path=/var/www/html --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASS} --dbhost=mariadb --allow-root
-wp core install --url=${DOMAIN_NAME} --title=inception --admin_user=${WP_ADM} --admin_password=${WP_ADM_PASS} --admin_email=${WP_ADM_EMAIL} --path=/var/www/html --allow-root
-wp user create ${WP_COMMOM_USER} ${WP_COMMOM_USER_EMAIL} --user_pass=${WP_COMMOM_USER_PASS} --allow-root --path=/var/www/html
+wp core install --url=https://${DOMAIN_NAME} --title=inception --admin_user=${WP_ADM} --admin_password=${WP_ADM_PASS} --admin_email=${WP_ADM_EMAIL} --path=/var/www/html --allow-root
+wp user create ${WP_COMMOM_USER} ${WP_COMMOM_USER_EMAIL} --user_pass=${WP_COMMOM_USER_PASS} --path=/var/www/html --allow-root 
+wp theme activate twentytwentyfour --allow-root
